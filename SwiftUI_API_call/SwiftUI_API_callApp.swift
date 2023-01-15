29d2933ea54faec91be4fa878a044c0bb63bd704
damian <skowronskid@icloud.com>
2023-01-15 15:52:17 +0100

    first commit

---
+++ SwiftUI_API_call/SwiftUI_API_callApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_API_callApp.swift
+//  SwiftUI_API_call
+//
+//  Created by Damian Skowroński on 15/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_API_callApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
