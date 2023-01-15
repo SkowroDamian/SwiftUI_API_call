14101869ec6eec29a3ad6582aa58c53091927e8b
damian <skowronskid@icloud.com>
2023-01-15 19:39:54 +0100

    app is fetching data and images form api

--- SwiftUI_API_call/ContentView.swift
+++ SwiftUI_API_call/ContentView.swift
@@ -4,18 +4,66 @@
 //
 //  Created by Damian Skowroński on 15/01/2023.
 //
+//TODO: caching images??
+//TODO: move fetchData to new ViewModel
 
 import SwiftUI
 
-struct ContentView: View {
+struct URLImage: View {
+    let urlString: String
+    
+    @State var data: Data?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        if let data = data, let uiimage = UIImage(data: data) {
+            Image(uiImage: uiimage)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 130, height: 70)
+                .background(Color.gray)
         }
-        .padding()
+        else {
+            Image(systemName: "video")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 130, height: 70)
+                .background(Color.gray)
+                .onAppear {
+                    fetchData()
+                }
+        }
+    }
+    
+    private func fetchData() {
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, _ in self.data = data }
+        task.resume()
+    }
+}
+
+struct ContentView: View {
+    @StateObject var viewModel = ViewModel()
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(viewModel.courses, id: \.self) { course in
+                    HStack {
+                        URLImage(urlString: course.image)
+                            
+                        Text(course.name)
+                            .bold()
+                    }
+                }
+            }
+            .navigationTitle("Courses")
+            .onAppear {
+                viewModel.fetch()
+            }
+        }
     }
 }
 
